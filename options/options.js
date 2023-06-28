55df25561b026024af13e282dc6baf2809418f41
Crowdbotics <team@crowdbotics.com>
2023-06-28 07:25:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "round-dream-41775",
+  url: "https://round-dream-41775.botics.co",
+  api: "https://round-dream-41775.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
